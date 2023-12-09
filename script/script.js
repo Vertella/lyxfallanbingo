bb193e0051143c8b0a5b8bda19428ad4a16fd0cd
Vertella <128084486+Vertella@users.noreply.github.com>
2023-12-09 03:00:38 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,22 +1,21 @@
-// target the bingo card with js
-// make for loop to target every single child node
-// retrieve nr of child in bingo card for nr of loops
-// target specific coins 
-// if i == 12 ignore
-async function bingoPrompts() {
-    const response = await fetch('./lyx.json');
-    const prompts = await response.json();
-    const prompt = prompts[Math.floor(Math.random() * prompts.length)]
-    return prompt;
-}
 
-document.addEventListener("DOMContentLoaded", function() {
-    bingoPrompts().then(prompt => {
-        var elements = document.querySelectorAll('.bingo-text');
-        for(var i = 0; i < elements.length; i++){
-            var str = elements[i].innerHTML;
-            elements[i].innerHTML = str.replace("",prompt)
-        }
-    })
+let prompts;
+
+fetch('./lyx.json')
+    .then(response => response.json())
+    .then(data => {
+    prompts = data;
+
+    const randomIndex = Math.floor(Math.random() * prompts.length);
+    const randomPrompt = prompts[randomIndex];
+
+    document.addEventListener("DOMContentLoaded", function() {
+        bingoPrompts().then(prompt => {
+            var elements = document.querySelectorAll('.bingo-text');
+            for(var i = 0; i < elements.length; i++){
+                var str = elements[i].innerHTML;
+                elements[i].innerHTML = str.replace("",randomPrompt)
+            }
+        });
+    });
 })
-
